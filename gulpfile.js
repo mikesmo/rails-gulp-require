58af22a17df84fc7ad4e683f50afe97fc13f84b4
michael <mike_smo@yahoo.com>
2014-12-03 18:21:09 +1100

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+var version = '0.0.0'
+
 require("coffee-script/register");
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
@@ -6,44 +8,29 @@ var coffee = require('gulp-coffee');
 var amdOptimize = require("amd-optimize");
 var es = require('event-stream');
 
-var dest = 'public/';
-var vendor = '/public/vendor/';
+var src = 'app/assets/javascripts/';
+var dest = 'public/' + version;
+var javascriptDest = dest + '/javascripts';
 
-
-var options = {
-    umd: false
-};
-
-gulp.task('configToJs', function () {
-    gulp.src('./app/assets/javascripts/main.coffee')
-        .pipe(coffee())
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('configEval', function () {
-    var configCoffee = require('./config.coffee');
-    var config = configCoffee.requireConfig();
-    console.log(JSON.stringify(config));
-    console.log(config.paths.jquery);
-});
 
 gulp.task('scripts', function () {
-    var main = require('./app/assets/javascripts/main.coffee');
+
+    var main = require('./' + src + 'main.coffee');
     var amdConfig = main.amdConfig();
-    amdConfig.baseUrl = 'app/assets/javascripts';
+    amdConfig.baseUrl = src;
     amdConfig.findNestedDependencies = true;
 
-    var javaScriptFromCoffeeScript = gulp.src('app/assets/javascripts/**/*.coffee')
-       .pipe(coffee());
-
-    var requirejs = gulp.src('app/assets/javascripts/vendor/requirejs/require.js')
-        .pipe(concat('require.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(dest + 'javascripts'));
-
-    return es.merge(javaScriptFromCoffeeScript)
+    var javaScriptFromCoffeeScript = gulp.src(src + '/**/*.coffee')
+        .pipe(coffee())
         .pipe(amdOptimize('main', amdConfig))
-        .pipe(concat('application.min.js'))
+        .pipe(concat('application.min.js'));
+
+
+    var requirejs = gulp.src(src + 'vendor/requirejs/require.js')
+        .pipe(concat('require.min.js'));
+
+
+    return es.merge(requirejs, javaScriptFromCoffeeScript)
         .pipe(uglify())
-        .pipe(gulp.dest(dest + 'javascripts'));
+        .pipe(gulp.dest(javascriptDest));
 });
